d27e74c0ce5b3b3dd7c4d796cee4eeecdf3b425e
Rodrigo Bossini <professorbossini@gmail.com>
2023-10-11 18:13:48 -0300

    log post result

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const handleAddBook = async () => {
     try {
-      await axios.post("http://" + url + '/livros', { titulo: title, autor: author, edicao: edition });
+      console.log('post result', await axios.post("http://" + url + '/livros', { titulo: title, autor: author, edicao: edition }));
       console.log (title, edition, author)
       setTitle('');
       setAuthor('');
